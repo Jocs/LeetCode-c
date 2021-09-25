0fc3d9607bbf78317908a35c01a416e58c816a3a
jocs <ransixi@gmail.com>
2021-09-26 00:01:40 +0800

    Solve 104

---
+++ 104.二叉树的最大深度.c
@@ -0,0 +1,56 @@
+/*
+ * @lc app=leetcode.cn id=104 lang=c
+ *
+ * [104] 二叉树的最大深度
+ *
+ * https://leetcode-cn.com/problems/maximum-depth-of-binary-tree/description/
+ *
+ * algorithms
+ * Easy (76.58%)
+ * Likes:    972
+ * Dislikes: 0
+ * Total Accepted:    512.6K
+ * Total Submissions: 668.9K
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * 给定一个二叉树，找出其最大深度。
+ * 
+ * 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+ * 
+ * 说明: 叶子节点是指没有子节点的节点。
+ * 
+ * 示例：
+ * 给定二叉树 [3,9,20,null,null,15,7]，
+ * 
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ * 
+ * 返回它的最大深度 3 。
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+int max(int a, int b) {
+  return a > b ? a : b;
+}
+
+int maxDepth(struct TreeNode* root){
+  if (!root) {
+    return 0;
+  }
+  return 1 + max(maxDepth(root->left), maxDepth(root->right));
+}
+// @lc code=end
+
